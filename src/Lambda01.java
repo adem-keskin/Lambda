a544bb882f3e812d57ef7e0a1938071b9dc4117b
adem-keskin <ademkeskin.de@gmail.com>
2022-11-25 20:24:52 +0100

    25.11.2022

---
+++ src/Lambda01.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Lambda01 {
+    public static void main(String[] args) {
+        /*
+      1)  Lambda --> mathod create  etme değil mevcut method'ları(library)secip kullanmaktır...
+          Lambda'lar sayesinde daha az kod ve hızlı geliştirme sağlanabilmektedir.
+      2) "Functional Programming" de "Nasil yaparim?" degil "Ne yaparim?" dusunulur.
+          "Structured Programming" de "Ne yaparim?" dan cok "Nasil Yaparim?" dusunulur
+      3)  "Functional Programming" hiz, code kisaligi, code okunabilirligi
+          ve hatasiz code yazma acilarindan cok faydalidir.
+      4)  Lambda sadece collections'larda(List, Queue ve Set) ve array'lerde kullanilabilir ancak map'lerde kullanılmaz.
+
+    */
+
+
+        List<Integer> sayi=new ArrayList<>(Arrays.asList(34,22,16,11,35,20,63,21,65,44,66,64,81,38,15));
+
+    }
+    //TASK: "Structured Programming" kullanarak list elemanlarını aynı satirda aralarında bosluk olacak sekilde print ediniz.
+
+
+
+
+
+
+
+}
